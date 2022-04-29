db6306100e1f1597baa3a9388007f7bd954361a6
iddimsangi <adammsangi@gmail.com>
2022-04-29 12:11:43 +0300

    improve: add test

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -1,11 +1,27 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import AddInput from "../AddInput";
 
 const mockedSetTodos = jest.fn();
 describe("AddInput tests", () => {
   test("Should renders input placeholder text", () => {
-    render(<AddInput todos={mockedSetTodos} />);
+    render(<AddInput todos={[]} setTodos={mockedSetTodos} />);
     const inputEl = screen.getByPlaceholderText(/Add a new task here.../i);
     expect(inputEl).toBeInTheDocument();
+  });
+
+  test("Should type input value ", () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodos} />);
+    const inputEl = screen.getByPlaceholderText(/Add a new task here.../i);
+    fireEvent.change(inputEl, { target: { value: "Go playing footbal" } });
+    expect(inputEl.value).toBe("Go playing footbal");
+  });
+
+  test("Should empty input value when add button is clicked ", () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodos} />);
+    const inputEl = screen.getByPlaceholderText(/Add a new task here.../i);
+    const buttonElement = screen.getByRole("button", { name: /Add/i });
+    fireEvent.change(inputEl, { target: { value: "Go playing footbal" } });
+    fireEvent.click(buttonElement);
+    expect(inputEl.value).toBe("");
   });
 });
