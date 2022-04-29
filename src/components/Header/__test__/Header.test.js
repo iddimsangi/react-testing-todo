22ba30c9bad5101ceaaca810bd09bf00082e5e5e
iddimsangi <adammsangi@gmail.com>
2022-04-29 10:05:17 +0300

    improve: add describe block

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,28 +1,30 @@
 import { screen, render } from "@testing-library/react";
 import Header from "../Header";
 
-test("should renders header text passed as a prop", () => {
-  render(<Header title="Test header message" />);
-  const headerTxt = screen.getByText(/test header message/i);
-  expect(headerTxt).toBeInTheDocument();
-});
-
-test("should renders header text", () => {
-  render(<Header title="Test header message" />);
-  const headerTxt = screen.getByRole("heading", {
-    name: "Another text",
+describe("Header tests", () => {
+  test("should renders header text passed as a prop", () => {
+    render(<Header title="Test header message" />);
+    const headerTxt = screen.getByText(/test header message/i);
+    expect(headerTxt).toBeInTheDocument();
   });
-  expect(headerTxt).toBeInTheDocument();
-});
 
-test("should not renders  header text list toddo", () => {
-  render(<Header title="Test header message" />);
-  const headerTxt = screen.queryByText("list of toddo");
-  expect(headerTxt).not.toBeInTheDocument();
-});
+  test("should renders header text", () => {
+    render(<Header title="Test header message" />);
+    const headerTxt = screen.getByRole("heading", {
+      name: "Another text",
+    });
+    expect(headerTxt).toBeInTheDocument();
+  });
 
-test("should renders all headings", () => {
-  render(<Header title="Test header message" />);
-  const headers = screen.getAllByRole("heading");
-  expect(headers.length).toBe(2);
+  test("should not renders  header text list toddo", () => {
+    render(<Header title="Test header message" />);
+    const headerTxt = screen.queryByText("list of toddo");
+    expect(headerTxt).not.toBeInTheDocument();
+  });
+
+  test("should renders all headings", () => {
+    render(<Header title="Test header message" />);
+    const headers = screen.getAllByRole("heading");
+    expect(headers.length).toBe(2);
+  });
 });
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,9 +10,17 @@ const MockedTodoFooter = ({ numberOfIncompleteTasks }) => {
   );
 };
 
-test("Should renders number of incomplete tasks", () => {
-  render(<MockedTodoFooter numberOfIncompleteTasks={7} />);
-  const paragraphElement = screen.getByText(/7 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-  expect(screen.getByText(/Followers/i)).toBeInTheDocument();
+describe("TodoFooter tests", () => {
+  test("Should renders number of incomplete tasks", () => {
+    render(<MockedTodoFooter numberOfIncompleteTasks={7} />);
+    const paragraphElement = screen.getByText(/7 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+    expect(screen.getByText(/Followers/i)).toBeInTheDocument();
+  });
+  test("Should renders number of incomplete task", () => {
+    render(<MockedTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+    expect(screen.getByText(/Followers/i)).toBeInTheDocument();
+  });
 });
